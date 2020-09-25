0bcb19763b81f1b28db0a9ba0f1f9e37d4f2229d
Alberto <A01207490@itesm.mx>
2020-09-25 14:46:04 -0500

    Creación del modelo de sesions y demas. Definición de relaciones entre las tablas.

---
+++ pasaporte/database/factories/SesionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Sesion;
+use Faker\Generator as Faker;
+
+$factory->define(Sesion::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
