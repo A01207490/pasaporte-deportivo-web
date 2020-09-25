6bbcd9e6eb725663f0898de8110eab36d3819075
Alberto <A01207490@itesm.mx>
2020-09-25 14:48:54 -0500

    Creación de factory de sesion funcional, crea users y clases, y clases crea coaches-

--- pasaporte/database/factories/SesionFactory.php
+++ pasaporte/database/factories/SesionFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Sesion::class, function (Faker $faker) {
     return [
-        //
+        'clase_id' => factory(\App\Clase::class),
+        'user_id' => factory(\App\User::class),
     ];
 });
