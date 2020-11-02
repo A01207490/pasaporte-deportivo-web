cc3bf4224cac9de6c1ee848f461044b1caf96c43
Alberto <A01207490@itesm.mx>
2020-11-02 14:13:39 -0600

    Tabla de modulo coach con sort, filter y paginación

---
+++ pasaporte/app/Tables/CoachTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tables;
+
+use App\Coach;
+use Okipa\LaravelTable\Abstracts\AbstractTable;
+use Okipa\LaravelTable\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class CoachTable extends AbstractTable
+{
+    /**
+     * Configure the table itself.
+     *
+     * @return \Okipa\LaravelTable\Table
+     * @throws \ErrorException
+     */
+    protected function table(): Table
+    {
+        return (new Table)->model(Coach::class)
+            ->routes([
+                'index'   => ['name' => 'coaches.index'],
+                'show' => ['name' => 'coaches.show'],
+                'create'  => ['name' => 'coaches.create'],
+                'edit'    => ['name' => 'coaches.edit'],
+                'destroy' => ['name' => 'coaches.confirm'],
+            ]);
+      
+    }
+
+    /**
+     * Configure the table columns.
+     *
+     * @param \Okipa\LaravelTable\Table $table
+     *
+     * @throws \ErrorException
+     */
+    protected function columns(Table $table): void
+    {
+        $table->column('coach_nombre')->title(__('Name'))->sortable()->searchable();
+        $table->column('coach_nomina')->title(__('Id'))->sortable()->searchable();
+        $table->column('coach_correo')->title(__('Email'))->sortable()->searchable();
+    }
+
+    /**
+     * Configure the table result lines.
+     *
+     * @param \Okipa\LaravelTable\Table $table
+     */
+    protected function resultLines(Table $table): void
+    {
+        //
+    }
+}
--- pasaporte/routes/web.php
+++ pasaporte/routes/web.php
@@ -46,7 +46,7 @@ Route::get("/qr/{coach}/download", "CoachController@download")->name('qr.downloa
 
 Route::delete("/anuncios/{anuncio}/confirm", "AnuncioController@confirm")->name('anuncios.confirm')->middleware(['auth', 'auth.admin']);
 Route::get("/clases/{clase}/confirm", "ClaseController@confirm")->name('clases.confirm')->middleware(['auth', 'auth.admin']);
-Route::get("/coaches/{coach}/confirm", "CoachController@confirm")->name('coaches.confirm')->middleware(['auth', 'auth.admin']);
+Route::delete("/coaches/{coach}/confirm", "CoachController@confirm")->name('coaches.confirm')->middleware(['auth', 'auth.admin']);
 Route::delete("/cs/{user}/confirm", "UsersController@confirm")->name('users.confirm')->middleware(['auth', 'auth.admin']);
 Route::get("/sesions/{sesion}/confirm", "SesionController@confirm")->name('sesions.confirm')->middleware(['auth', 'auth.admin']);
 
